558a09edb1bebe26eeb62c8bc4fcf25ba4d0e974
Yeongjae Park <62601806+youngpark-POS@users.noreply.github.com>
2026-02-11 18:48:30 +0900

    redundent code line removed (views.py::line 27)

--- smtsgg/views.py
+++ smtsgg/views.py
@@ -7,23 +7,24 @@ from .models import SearchCount
 from .riotapi import *
 from pprint import pprint
 
-'''
+"""
 
 TODOs
 
 Introduce error handling process
 
-'''
+"""
 
 
 def index(request):
     return render(request, "smtsgg/index.html")
 
+
 def search(request):
     nickname, tag = request.POST.get("playerName").split("#")
     tag = "KR1" if tag == "" else tag
     try:
-        puuid = get_puuid(nickname, tag)
+        # puuid = get_puuid(nickname, tag)
         # record_list = SearchCount.objects.filter(puuid=puuid)
         # if len(record_list) == 0:
         #     new_record = SearchCount(puuid=puuid, gamename=nickname, tagline=tag)
@@ -37,8 +38,9 @@ def search(request):
     # get user info from API query
     except KeyError:
         raise Http404("Summoner name not found")
-    except Exception:
-        raise Http404("Unknown error")
+    except Exception as e:
+        raise Http404(e)
+
 
 def detail(request, nickname_and_tag):
     nickname, tag = nickname_and_tag.split("#")
@@ -49,10 +51,16 @@ def detail(request, nickname_and_tag):
 
     context = {
         "nickname_and_tag": nickname_and_tag,
-        "current_rank_solo" : " ".join([rank_info["solo"]["tier"], rank_info["solo"]["rank"]]),
-        "current_LP_solo" : str(rank_info["solo"]["leaguePoints"]) + ("" if rank_info["solo"]["tier"] == "Unranked" else "LP"),
-        "current_rank_flex" : " ".join([rank_info["flex"]["tier"], rank_info["flex"]["rank"]]),
-        "current_LP_flex" : str(rank_info["flex"]["leaguePoints"]) + ("" if rank_info["flex"]["tier"] == "Unranked" else "LP"),
+        "current_rank_solo": " ".join(
+            [rank_info["solo"]["tier"], rank_info["solo"]["rank"]]
+        ),
+        "current_LP_solo": str(rank_info["solo"]["leaguePoints"])
+        + ("" if rank_info["solo"]["tier"] == "Unranked" else "LP"),
+        "current_rank_flex": " ".join(
+            [rank_info["flex"]["tier"], rank_info["flex"]["rank"]]
+        ),
+        "current_LP_flex": str(rank_info["flex"]["leaguePoints"])
+        + ("" if rank_info["flex"]["tier"] == "Unranked" else "LP"),
         "match_list": [],
         "mastery_list": [],
     }
@@ -63,6 +71,7 @@ def detail(request, nickname_and_tag):
     context["mastery_list"] = get_champion_mastery(puuid)
 
     return render(request, "smtsgg/detail.html", context=context)
+
 
 def match_detail(request, match_id):
     match_infos = get_match_detail(match_id)
